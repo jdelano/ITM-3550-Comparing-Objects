f67f657246c46d9a87c2c3feecd297c67f974cc5
jdelano <jdelano@cedarville.edu>
2024-02-21 14:15:24 -0500

    Demo Finished

---
+++ ComparingObjects2024/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace ComparingObjects2024
+{
+	public class Person : IEquatable<Person>
+	{
+		public string Name { get; set; } = string.Empty;
+		public int Age { get; set; }
+        public string Street { get; set; } = string.Empty;
+
+        public bool Equals(Person? other)
+        {
+            if (other is null) return false;
+            return Name == other.Name && Age == other.Age && Street == other.Street;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as Person);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Name + Age.ToString()).GetHashCode();
+        }
+
+        public static bool operator ==(Person rhs, Person lhs)
+        {
+            return rhs.Equals(lhs);
+        }
+
+        public static bool operator !=(Person rhs, Person lhs)
+        {
+            return !rhs.Equals(lhs);
+        }
+
+    }
+}
+
---
+++ ComparingObjects2024/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ComparingObjects2024;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x = 5;
+        int y = 5;
+        if (x == y)
+        {
+            Console.WriteLine("x and y are equal.");
+        }
+
+        string s1 = "hello";
+        string s2 = "hello";
+        if (s1 == s2)
+        {
+            Console.WriteLine("s1 is equal to s2.");
+        }
+
+        Person p1 = new Person { Name = "John", Age = 30 };
+        Person p2 = new Person { Name = "John", Age = 30 };
+        if (p1 == p2) //(p1.Equals(s1))
+        {
+            Console.WriteLine("Person objects are EQUAL.");
+        }
+        else
+        {
+            Console.WriteLine("Person objects are DIFFERENT!");
+        }
+    }
+}
+
